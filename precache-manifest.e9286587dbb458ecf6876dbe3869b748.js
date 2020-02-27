aa9f55634c4c6d8dd2fa176658ade122012c0207
Liam Southall <lsouthall116@gmail.com>
2020-02-27 14:08:13 +0000

    Updates

---
+++ precache-manifest.e9286587dbb458ecf6876dbe3869b748.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf1975998f2398927ae8f54ff960dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffe2b2d7429f14d627f1",
+    "url": "/static/css/main.e1d844ae.chunk.css"
+  },
+  {
+    "revision": "b00ae208b269c0e6fc04",
+    "url": "/static/js/2.671b1be6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.671b1be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe2b2d7429f14d627f1",
+    "url": "/static/js/main.be80f81f.chunk.js"
+  },
+  {
+    "revision": "d62a8d0745e273675d8b",
+    "url": "/static/js/runtime-main.587e4aae.js"
+  },
+  {
+    "revision": "2a72b74a74258d99a08964d2bc908853",
+    "url": "/static/media/babymario.2a72b74a.jpg"
+  },
+  {
+    "revision": "c3a8ee939ff4c23a6f5cdbc066bcc01c",
+    "url": "/static/media/boo.c3a8ee93.jpg"
+  },
+  {
+    "revision": "69f44bd2e7c7279b1925d677e673c54f",
+    "url": "/static/media/bowser.69f44bd2.jpg"
+  },
+  {
+    "revision": "1eaaa11a43d74c2ddd60bd1f73cb0065",
+    "url": "/static/media/marioBros.1eaaa11a.jpg"
+  },
+  {
+    "revision": "c26b3986c52b825fcab42136d4028ece",
+    "url": "/static/media/toad.c26b3986.jpg"
+  },
+  {
+    "revision": "93b6971ad869d31fba05e135cc1baaf7",
+    "url": "/static/media/waluigi.93b6971a.jpg"
+  },
+  {
+    "revision": "174f5b169e217be5ca3332f3dedccb7c",
+    "url": "/static/media/wario.174f5b16.jpg"
+  },
+  {
+    "revision": "3ec7bdf1f3b7f823fe5dd3133a8a24df",
+    "url": "/static/media/yoshi.3ec7bdf1.jpg"
+  }
+]);
